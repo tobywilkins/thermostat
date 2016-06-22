11fcde0dabf0f2a09842d3e7ffac17de44b88c55
Rb2030 <rossmail2050@gmail.com>
2016-06-22 12:13:01 +0100

    Setting temperature and added ten degrees

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -6,4 +6,9 @@ describe('Feature Test:', function() {
   it('starts at 20*', function() {
     expect(thermostat.temperature()).toEqual(20);
   });
+
+  it('can change the temperature', function() {
+    thermostat.changeTemperature(10);
+    expect(thermostat.temperature()).toEqual(30);
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,8 +2,13 @@
 
 function Thermostat () {
   this._temperature = 20;
-
-  Thermostat.prototype.temperature = function() {
-    return this._temperature;
-  };
 };
+  Thermostat.prototype = {
+    temperature: function() {
+      return this._temperature;
+    },
+
+    changeTemperature: function(change) {
+      this._temperature = this._temperature + change;
+    }
+  };
